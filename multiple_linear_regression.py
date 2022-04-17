cf6851c4efb20e7a305ac948f0e42dd1dd6dc0a9
OzanberkZ79 <62771764+OzanberkZ79@users.noreply.github.com>
2022-04-17 23:38:07 +0300

    Add files via upload

---
+++ multiple_linear_regression.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # MULTIPLE LINEAR REGRESSION
+# 
+# Multiple Linear Regression'da birden fazla bağımsız(independent) değişkene karşılık bir bağımlı(dependent) dğeişken bulunur.
+# 
+# Linear Regression veriler arasında var olan korelasyonu(ilişkiyi) kullanarak yeni gelecek verileri tahmin etme modelidir. Burada makine öğrenimi bize veriler arasındaki bu ilişkiyi belirlememize yardımcı olur ve bu sayede yeni verileri tahmin edebiliriz. 
+
+
+
+
+import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+# sklearn library
+from sklearn import linear_model
+import pickle
+
+
+mymodel=pickle.load(open("fiyatlistesimodeli.pickle",'rb'))
+
+
+
+
+
+ram = int(input("Ram değeri giriniz: "))
+
+cpu=int(input("Cpu değeri giriniz: "))
+
+ssd=int(input("SSD değeri giriniz: "))
+
+
+
+
+y = mymodel.predict([[ram,cpu,ssd]])
+
+print("Fiyat: ",y)
+
+
+
+
+
+
